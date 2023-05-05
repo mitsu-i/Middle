0c95154cf102e015894b5240b5b2f4c374ef50a4
mitsu-i <mitsui116.a@gmail.com>
2023-05-05 09:23:05 +0900

    新規作成

---
+++ src/main/java/plugin/middle/setLevel30.java
@@ -0,0 +1,22 @@
+package plugin.middle;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class setLevel30 implements CommandExecutor {
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (sender instanceof Player player){
+      if(args.length == 1){
+        player.setLevel(Integer.parseInt(args[0]));
+      }else {
+        player.sendMessage("Nooooo!");
+      }
+
+    }
+    return false;
+  }
+}
